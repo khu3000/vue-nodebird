135605d3b4aeefc2eaa4f4252a176c0a0d94d0fd
khu3000 <khu3000@naver.com>
2020-12-01 23:43:29 +0900

    ifinite 스크롤 준비

--- ch1/front/store/posts.js
+++ ch1/front/store/posts.js
@@ -1,6 +1,9 @@
 export const state = () =>({
     mainPosts:[],
+    hasMorePost : true, //다음 데이터가 있는 경우 true
 });
+
+const limit = 10; //건당 가져올 갯수
 
 export const mutations = {
     addMainPost(state, payload){
@@ -13,7 +16,21 @@ export const mutations = {
     addComment(state, payload){
         const index = state.mainPosts.findIndex(v=>v.id === payload.postId);
         state.mainPosts[index].comments.unshift(payload);
-    }
+    },
+    loadPosts(state){
+        const fakePosts = Array(limit).fill().map( v => ({
+            id : Math.random().toString(),
+            user: {
+                id:1,
+                nickname: '꿀꿀',
+            },
+            content: `Hello infinite scrolling~ ${Math.random()}`,
+            comments:[],
+            images : [],
+        }));
+        state.mainPosts = state.mainPosts.concat(fakePosts);
+        state.hasMorePost = fakePosts.length === limit;
+    },
 };
 
 export const actions = {
@@ -25,5 +42,10 @@ export const actions = {
     },
     addComment({commit}, payload){
         commit('addComment', payload);
+    },
+    loadPosts({commit, state}, payload){
+        if(state.hasMorePost){
+            commit('loadPosts');
+        }
     }
 }
